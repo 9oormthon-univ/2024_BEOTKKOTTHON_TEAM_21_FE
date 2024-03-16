e0ec2564473238ee100782a8ee00be1a85efb30b
dalsu0222 <dalsu0222@sju.ac.kr>
2024-03-16 22:47:17 +0900

    feat: navigation 좌측 drawer 구현

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,14 +10,123 @@ const NavContainer = styled.div`
   min-height: 10vh;
 `;
 
+const DrawerProfileContainer = styled.div`
+  height: 180px;
+  background: linear-gradient(180deg, #ffd875 0%, #ffa680 100%);
+`;
+
 const Navbar = () => {
   const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const goToBack = () => {
     navigate(-1);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      const sidebar = document.getElementById("sidebar");
+      const openSidebarButton = document.getElementById("open-sidebar");
+      if (
+        sidebar &&
+        !sidebar.contains(e.target) &&
+        !openSidebarButton.contains(e.target)
+      ) {
+        setIsSidebarOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div>
+      <div
+        id="sidebar"
+        className={`absolute bg-white  w-64 min-h-screen overflow-y-auto transition-transform transform ${
+          isSidebarOpen ? "" : "-translate-x-full"
+        } ease-in-out duration-300`}
+        style={{ zIndex: 1000 }}
+      >
+        <div>
+          <div>
+            <DrawerProfileContainer>
+              <div className="p-5 h-full flex flex-col justify-between">
+                <button onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="9"
+                    height="13"
+                    viewBox="0 0 9 13"
+                    fill="none"
+                  >
+                    <path
+                      d="M8 1L1 6.5L8 12"
+                      stroke="#878787"
+                      stroke-linecap="round"
+                    />
+                  </svg>
+                </button>
+                <div className="flex">
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="50"
+                      height="50"
+                      viewBox="0 0 50 50"
+                      fill="none"
+                    >
+                      <circle
+                        cx="24.5207"
+                        cy="24.5207"
+                        r="24.5207"
+                        fill="white"
+                      />
+                      <circle
+                        cx="24.5206"
+                        cy="16.6919"
+                        r="7.47045"
+                        fill="#D7D7D7"
+                      />
+                      <path
+                        d="M10.8981 36.4666C10.8981 30.6419 15.6199 25.9201 21.4446 25.9201H27.5968C33.4214 25.9201 38.1433 30.6419 38.1433 36.4666C38.1433 37.9228 36.9628 39.1033 35.5067 39.1033H13.5347C12.0786 39.1033 10.8981 37.9228 10.8981 36.4666Z"
+                        fill="#D7D7D7"
+                      />
+                    </svg>
+                  </div>
+                  <div className="mx-3 text-white">
+                    <div className="text-lg">미르미</div>
+                    <div className="text-sm text-white">goorumi@goorm.com</div>
+                  </div>
+                </div>
+              </div>
+            </DrawerProfileContainer>
+            <div>
+              <ul>
+                <li className="my-5 px-5 h-10 border-b border-gray-200">
+                  <a href="#" className="block hover:text-primary">
+                    설정
+                  </a>
+                </li>
+                <li className="my-5 px-5 h-10 border-b border-gray-200">
+                  <a href="#" className="block hover:text-primary">
+                    도움말
+                  </a>
+                </li>
+                <li className="my-5 px-5 h-10 border-b border-gray-200">
+                  <a href="#" className="block hover:text-primary">
+                    로그아웃
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
       <NavContainer>
         <button
           className="flex justify-center items-center ml-2 w-10 rounded "
@@ -33,7 +142,11 @@ const Navbar = () => {
             <path d="M8 1L1 6.5L8 12" stroke="#878787" stroke-linecap="round" />
           </svg>
         </button>
-        <button className="mr-4">
+        <button
+          className="mr-4"
+          id="open-sidebar"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="30"
