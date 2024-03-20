594d81e962085b03c9da716bd84fce8f805ef546
dalsu0222 <dalsu0222@sju.ac.kr>
2024-03-21 01:19:33 +0900

    comment: 콘솔출력부분 주석 처리

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -80,7 +80,7 @@ const Navbar = () => {
             Authorization: `Bearer ${authToken}`,
           },
         });
-        console.log(response.data);
+        // console.log(response.data);
         if (response.status === 200) {
           setUserData(response.data);
 
@@ -100,7 +100,7 @@ const Navbar = () => {
     fetchUserData();
   }, []);
 
-  console.log(email, id, nickname, profileImgUrl);
+  // console.log(email, id, nickname, profileImgUrl);
 
   return (
     <div>
